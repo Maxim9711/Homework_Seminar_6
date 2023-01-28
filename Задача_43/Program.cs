84cb15baa43f00ccd93607af5041f7f16407987b
Maxim <Bruce.97@mail.ru>
2023-01-28 12:51:54 +0700

    task 43

---
+++ Задача_43/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите k1: ");
+float k1 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите b1: ");
+float b1 = Convert.ToSingle(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+float k2 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Введите b2: ");
+float b2 = Convert.ToSingle(Console.ReadLine());
+
+
+float x = (-b1 + b2)/(k1-k2);
+float y = k1*x+b1;
+
+Console.Write($"Точка пересечения прямых [{y},{x}]");
