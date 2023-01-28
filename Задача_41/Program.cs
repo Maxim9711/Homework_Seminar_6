254a8f1d3ead532fc6315c7604b01b1f36102781
Maxim <Bruce.97@mail.ru>
2023-01-28 11:58:23 +0700

    task 41

---
+++ Задача_41/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите количество вводимых чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [num];
+
+Console.Write("Введите числа: ");
+for (int i=0; i < array.Length; i++)
+  array[i] = Convert.ToInt32(Console.ReadLine());
+
+int numPositive = 0;
+for (int i = 0; i < array.Length; i++)
+  if (array[i] > 0) numPositive++;
+
+ Console.WriteLine($"Количество положительных чисел: {numPositive}");
